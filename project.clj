6ce4a3da151569cf5f0478a6beebfe0d08c16df8
Viktar Basharymau <viktar.basharymau@thehamon.com>
2013-08-04 20:18:57 +0300

    switching from lein to lein2 - fourth attempt

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lib-noir "0.6.6"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.1"]] :plugins [[lein-midje "3.1.1"]]}})
